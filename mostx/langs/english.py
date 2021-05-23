3a5a33fe0f86a81b00efdf53a9a64291c8834a91
gottadiveintopython <flow4re2c@gmail.com>
2021-05-23 09:16:47 +0900

    refactor the english module

--- mostx/langs/english.py
+++ mostx/langs/english.py
@@ -2,21 +2,10 @@ __all__ = ('get_max_adjectives', 'generate_statement', 'generate_question', )
 
 
 ADJ_TEXT = '''
-smaller,smallest/larger,largest
-longer,longest/shorter,shortest
-colder,coldest/hotter,hottest
-newer,newest/older,oldest
-faster,fastest/slower,slowest
-nearer,nearest/farther,farthest
-harder,hardest/softer,softest
-fatter,fattest/thinner,thinnest
-sadder,saddest/happier,happiest
-quieter,quietest/noisier,noisiest
+small/larg long/short cold/hott new/old fast/slow near/farth hard/soft
+fatt/thinn sadd/happi quiet/noisi
 '''
-ADJS = tuple(
-    tuple(map(lambda s: s.split(','), line.split(sep='/')))
-    for line in ADJ_TEXT.split()
-)
+ADJS = tuple(tuple(i.split(sep='/', maxsplit=1)) for i in ADJ_TEXT.split())
 
 
 def get_max_adjectives():
@@ -26,17 +15,19 @@ def get_max_adjectives():
 def generate_statement(a, b, adjpart):
     if len(adjpart) == 1:
         (index, is_fwd_direction) = adjpart[0]
-        return f'{a} is {ADJS[index][0 if is_fwd_direction else 1][0]} than {b}'
+        return r'{0} is {2}er than {1}'.format(
+            a, b, ADJS[index][0 if is_fwd_direction else 1],
+        )
     else:
         adjs = [
-            ADJS[index][0 if is_fwd_direction else 1][0]
+            ADJS[index][0 if is_fwd_direction else 1] + 'er'
             for (index, is_fwd_direction) in adjpart
         ]
-        lastadj = adjs.pop()
-        return f"{a} is {','.join(adjs)} and {lastadj} than {b}"
+        last_adj = adjs.pop()
+        return f"{a} is {','.join(adjs)} and {last_adj} than {b}"
 
 
 def generate_question(index, is_fwd_direction):
-    return 'Which is the {}?'.format(
-        ADJS[index][0 if is_fwd_direction else 1][1]
+    return r'Which is the {}est?'.format(
+        ADJS[index][0 if is_fwd_direction else 1]
     )
--- tests/langs/test_english.py
+++ tests/langs/test_english.py
@@ -1,7 +1,7 @@
 import pytest
 
 
-def test_get_max_adj():
+def test_get_max_adjectives():
     from mostx.langs.english import get_max_adjectives
     assert get_max_adjectives() >= 1
 
